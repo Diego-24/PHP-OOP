b9c82a7f77551e338757a0c3a56b67eb17fba243
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-01-14 16:17:00 -0800

    no message

--- inheritance.php
+++ inheritance.php
@@ -6,17 +6,13 @@
 		public $lastName;
 		public $gender;
 		public $age;
-		public $grade;
-		public $mood;
-
-		function __construct($firstName, $middleName, $lastName, $gender, $age, $grade, $mood) {
+		
+		function __construct($firstName, $middleName, $lastName, $gender, $age) {
 			$this->firstName = $firstName;
 			$this->middleName = $middleName;
 			$this->lastName = $lastName;
 			$this->gender = $gender;
 			$this->age = $age;
-			$this->grade = $grade;
-			$this->mood = $mood;
 		}
 
 		function getName() {
@@ -29,18 +25,26 @@
 	}
 
 	class Me extends Teen {
+		function __construct($firstName, $middleName, $lastName, $gender, $age, $mood) {
+			parent::__construct($firstName, $middleName, $lastName, $gender, $age);
+		$this->mood = $mood;
+		}
 		function greet() {
 			return $this->mood;
 		}
 	}
 
 	class Smartass extends Teen {
+		function __construct($firstName, $middleName, $lastName, $gender, $age, $grade) {
+			parent::__construct($firstName, $middleName, $lastName, $gender, $age);
+		$this->grade = $grade;
+		}
 		function smartness() {
 			return $this->grade;
 		}
 	}
 
-	$me = new Me("Diego", "A.", "Sanchez", "male", 15, "A", "happy");
+	$me = new Me("Diego", "A.", "Sanchez", "male", 15, true);
 	print "Teen 1 is = " . $me->getName();
 
 	/*Example 2*/
@@ -48,15 +52,11 @@
 		public $title;
 		public $category;
 		public $theater;
-		public $rating;
-		public $review;
 
-		function __construct($title, $category, $theater, $rating, $review) {
+		function __construct($title, $category, $theater) {
 			$this->title = $title;
 			$this->category = $category;
 			$this->theater = $theater;
-			$this->rating = $rating;
-			$this->review = $review;
 		}
 
 		function getTitle() {
@@ -68,18 +68,25 @@
 	}
 
 	class Favorite extends Movie {
+		function __construct($title, $category, $theater, $review) {
+			parent::__construct($title, $category, $theater);
+		$this->review = $review;
+		}
 		function opinion() {
 			return $this->review;
 		}
 	}
 
-	class Critc extends Movie {
+	class Critic extends Movie {
+		function __construct($title, $category, $theater, $rating) {
+			parent::__construct($title, $category, $theater);
+		$this->rating = $rating;
 		function restriction() {
 			return $this->rating;
 		}
 	}
 
-	$favorite = new Favorite("Guardians of the Galaxy", "action", "AMC", "pg-13", "5 stars");
+	$favorite = new Favorite("Guardians of the Galaxy", "action", "AMC", true);
 	print "Movie 1 = " . $favorite->getTitle();
 
 	/*Example 3*/
@@ -87,17 +94,13 @@
 		public $name;
 		public $creator;
 		public $group;
-		public $power;
-		public $weapons;
 		public $personality;
 		public $awesomeness;
 
-		function __construct($name, $creator, $group, $power, $weapons, $personality, $awesomeness) {
+		function __construct($name, $creator, $group, $personality, $awesomeness) {
 			$this->name = $name;
 			$this->creator = $creator;
 			$this->group = $group;
-			$this->power = $power;
-			$this->weapons = $weapons;
 			$this->personality = $personality;
 			$this->awesomeness = $awesomeness;
 		}
@@ -112,18 +115,24 @@
 	}
 
 	class SuperPower extends Superhero {
+		function __construct($name, $creator, $group, $power, $personality, $awesomeness) {
+			parent::__construct($name, $creator, $group, $personality, $awesomeness);
+		$this->power = $power;
 		function awesome() {
 			return $this->power;
 		}
 	}
 
 	class WeaponOfChoice extends Superhero {
+		function __construct($name, $creator, $group, $weapons, $personality, $awesomeness) {
+			parent::__construct($name, $creator, $group, $personality, $awesomeness);
+		$this->weapons = $weapons;
 		function ouch() {
 			return $this->weapons;
 		}
 	}
 
-	$superPower = new SuperPower("Deadpool", "Marvel", "X-men", "health regeneration","2 swords and 2 guns" , "hilarious and funny", 100);
+	$superPower = new SuperPower("Deadpool", "Marvel", "X-men", "hilarious and funny", 100, true);
 	print "Best Superhero = " . $superPower->getName();
 
 ?>
